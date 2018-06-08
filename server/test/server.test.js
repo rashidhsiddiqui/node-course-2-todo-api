a26bae38792c43196355f3c08052ad9e50bc979a
Rashid Siddiqui <rsh_bcs@hotmail.com>
2018-06-09 02:48:44 +0500

    Add test for GET /todos

--- server/test/server.test.js
+++ server/test/server.test.js
@@ -4,11 +4,18 @@ const request = require("supertest");
 var {app} = require("./../server.js");
 var {Todo} = require("./../models/todo.js");
 
+//For GET todos test
+const todos =[{
+  "text": "test todo 1"
+},{
+  "text": "test todo 2"
+}];
+
 //Make sure to run test case we are emptying the collection
 beforeEach((done) => {
   Todo.remove({}).then(() => {
-    done();
-  });
+    return Todo.insertMany(todos); //seed the data; Mongoose method
+  }).then(() => done());
 });
 
 describe("POST /todos", () => {
@@ -28,7 +35,7 @@ describe("POST /todos", () => {
         }
 
         //find if todo inserted
-        Todo.find().then((todos) => {
+        Todo.find({text}).then((todos) => {
           expect(todos.length).toBe(1); //should be only one todo in collection
           expect(todos[0].text).toBe(text);
           done();
@@ -49,11 +56,24 @@ describe("POST /todos", () => {
       }
 
       Todo.find().then((todos) => {
-        expect(todos.length).toBe(0);
+        expect(todos.length).toBe(2); //should be two todos in collection because of GET has seeded 2 todos
         done();
       }).catch((e) => {
         done(e);
       });
     });
+  });
+});
+
+describe("GET /todos", () => {
+
+  it("should return all todos", (done) => {
+    request(app)
+    .get("/todos")
+    .expect(200)
+    .expect((res) => {
+      expect(res.body.todos.length).toBe(2);
+    })
+    .end(done);
   });
 });
