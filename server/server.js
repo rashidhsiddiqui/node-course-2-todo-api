85068550f12b64d6800951d51f6928265ef3d068
Rashid Siddiqui <rsh_bcs@hotmail.com>
2018-06-21 03:49:48 +0500

    Add DELETE todos/:id route

--- server/server.js
+++ server/server.js
@@ -58,6 +58,27 @@ app.get("/todos/:id", (req, res) => {
   });
 });
 
+//Delete todos/id
+app.delete("/todos/:id", (req, res) => {
+
+  var id = req.params.id;
+
+  if(!ObjectID.isValid(id)){
+    return res.status(404).send();
+  }
+
+  Todo.findByIdAndRemove(id).then((todo) => {
+
+    if(!todo){
+      return res.status(404).send();
+    }
+
+    res.send({todo});
+  }).catch((e) => {
+    res.status(400).send(e);
+  });
+});
+
 app.listen(port, () => {
   console.log(`Started listening to port ${port}`);
 });
